f5a57b485eb8a4c665707161a76dfd671a5f41d6
Fernando Zelaya <fernando.zelaya@claro.com.ar>
2023-09-28 20:08:19 -0300

    Prueba de inicio

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -7,7 +7,6 @@ import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-
 @CucumberOptions(features = "src/test/resources",
         plugin = {"pretty"},
         tags = "@AltaUsuario", glue = {"Steps"})
